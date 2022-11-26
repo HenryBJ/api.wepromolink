b7b78c63d85fc0f4af941cf5e9f7f533f9cb583e
Jose Enrique <jbenitez@redgage.com>
2022-11-25 23:56:25 -0500

    Adding Nanoid

--- Models/SponsoredLinkModel.cs
+++ Models/SponsoredLinkModel.cs
@@ -3,9 +3,11 @@ namespace WePromoLink.Models;
 public class SponsoredLinkModel
 {
     public int Id { get; set; }
+    public string ExternalId { get; set; }
     public EmailModel Email { get; set; }
     public int EmailModelId { get; set; }
     public string Title { get; set; }
+    public string? Description { get; set; }
     public string Url { get; set; }
     public string? ImageUrl { get; set; }
     public decimal Budget { get; set; }
--- Services/SponsoredLinkService.cs
+++ Services/SponsoredLinkService.cs
@@ -1,10 +1,46 @@
+using WePromoLink.Data;
+using WePromoLink.Models;
+
 namespace WePromoLink.Services;
 
 public class SponsoredLinkService : ISponsoredLinkService
 {
-    public Task<string> CreateSponsoredLink(CreateSponsoredLink link)
+    private readonly DataContext _db;
+
+    public SponsoredLinkService(DataContext db)
     {
-        throw new NotImplementedException();
+        _db = db;
+    }
+    public async Task<string> CreateSponsoredLink(CreateSponsoredLink link)
+    {
+        var email = _db.Emails.Where(e=>e.Email.ToLower() == link.Email!.ToLower()).SingleOrDefault();
+        if(email == null)
+        {
+            email = new EmailModel{CreatedAt = DateTime.UtcNow, Email = link.Email!.ToLower()};
+            _db.Emails.Add(email);
+            await _db.SaveChangesAsync();
+        }
+
+        string  externalId = await Nanoid.Nanoid.GenerateAsync(size:12);
+
+         SponsoredLinkModel slink = new SponsoredLinkModel
+         {
+            ExternalId = externalId,
+            Budget = 0,
+            CreatedAt = DateTime.UtcNow,
+            EmailModelId = email.Id,
+            EPM = link.EPM,
+            ImageUrl = link.ImageUrl,
+            Title = link.Title!,
+            Description = link.Description,
+            RemainBudget = 0,
+            Url = link.Url!            
+         }; 
+
+         _db.SponsoredLinks.Add(slink);
+         await _db.SaveChangesAsync();
+
+         return externalId;
     }
 
     public Task FundSponsoredLink(string sponsoredLinkid)
